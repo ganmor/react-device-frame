6c43f54edca372cbbff336530a2803e9e6cf0a2a
Morgan Laupies <morgan.laupies@beedeez.com>
2018-10-25 17:41:52 +0200

    fixes

--- build/content-frame.js
+++ build/content-frame.js
@@ -1,10 +1,16 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -22,13 +28,13 @@ var ContentFrame = function (_React$Component) {
 	}
 
 	_createClass(ContentFrame, [{
-		key: "render",
+		key: 'render',
 		value: function render() {
-			return React.createElement("iframe", { src: this.props.src, name: this.props.name });
+			return _react2.default.createElement('iframe', { src: this.props.src, name: this.props.name });
 		}
 	}]);
 
 	return ContentFrame;
-}(React.Component);
+}(_react2.default.Component);
 
 exports.default = ContentFrame;
