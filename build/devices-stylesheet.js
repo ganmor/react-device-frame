06e5bb7e5af4d31d0162b68ea9cbfe23b683f3d1
Morgan Laupies <morgan.laupies@beedeez.com>
2018-11-14 17:10:52 +0100

    fixes

--- build/devices-stylesheet.js
+++ build/devices-stylesheet.js
@@ -1,46 +1,46 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getStyleSheet = exports.getProportionApplier = exports.palette = undefined;
 
-var _aphrodite = require("aphrodite");
+var _aphrodite = require('aphrodite');
 
 var palette = exports.palette = {
-  black: ["#000", "#111", "#1E1E1E", "#242324", "#3C3D3D"]
+  black: ['#000', '#111', '#1E1E1E', '#242324', '#3C3D3D']
 };
 
 var getProportionApplier = exports.getProportionApplier = function getProportionApplier() {
   var proportion = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
 
   return function (value) {
-    return Math.ceil(value * proportion);
+    return Math.ceil(value / proportion);
   };
 };
 
 var getStyleSheet = exports.getStyleSheet = function getStyleSheet() {
   var proportion = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
-  var color = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "black";
+  var color = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'black';
 
   var applyProportion = getProportionApplier(proportion);
   var shades = palette[color];
   return _aphrodite.StyleSheet.create({
     marvelDevice: {
-      display: "inline-block",
-      position: "relative",
-      boxSizing: "content-box"
+      display: 'inline-block',
+      position: 'relative',
+      boxSizing: 'content-box'
     },
     screen: {
-      width: "100%",
-      position: "relative",
-      height: "100%",
-      zIndex: "3",
-      background: "white",
-      overflow: "hidden",
-      display: "block",
+      width: '100%',
+      position: 'relative',
+      height: '100%',
+      zIndex: '3',
+      background: 'white',
+      overflow: 'hidden',
+      display: 'block',
       borderRadius: applyProportion(1),
-      boxShadow: "0 0 0 " + applyProportion(3) + "px " + shades[1]
+      boxShadow: '0 0 0 ' + applyProportion(3) + 'px ' + shades[1]
     },
     middleBar: {
       width: applyProportion(3),
@@ -48,13 +48,13 @@ var getStyleSheet = exports.getStyleSheet = function getStyleSheet() {
       top: 0,
       left: applyProportion(90),
       background: shades[0],
-      position: "absolute"
+      position: 'absolute'
     },
     bars: {
       height: applyProportion(3),
       background: shades[0],
-      width: "100%",
-      display: "block"
+      width: '100%',
+      display: 'block'
     }
   });
 };
